e36c966077d59d92b84a1137cb383f2451ba6e94
Splash-3d <lighting2385@gmail.com>
2026-01-01 14:11:38 +0100

    Fix ESLint warning and prepare production build
    - Remove unused 'handleAddToCart' parameter to fix ESLint warning
    - Remove client/build from .gitignore to allow production builds
    - Clean production build ready for deployment

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -65,7 +65,6 @@ const ProductsPage: React.FC = () => {
       precio: product.precio,
       imagen: product.imagen
     });
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     console.log('Product added to cart:', product.nombre);
   };
 
